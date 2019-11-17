aba8a742a172db3373381322ce98ccef49aaaed8
Kelly Selden <kellyselden@gmail.com>
2019-11-17 22:38:13 +0000

    supporting test modifications

--- test/tests.js
+++ test/tests.js
@@ -13,6 +13,7 @@ const {
   assertCodemodRan
 } = require('./helpers/assertions');
 const manifest = require('../manifest');
+const run = require('ember-cli-update/src/run');
 
 const codemods = Object.keys(manifest);
 
@@ -83,6 +84,10 @@ describe('runs codemods', function() {
         runCodemods: true,
         async beforeMerge() {
           await _merge('local', tmpPath);
+
+          if (manifest[codemod].script) {
+            await run('npm install', { cwd: tmpPath });
+          }
         }
       });
 
@@ -109,8 +114,13 @@ describe('runs codemods', function() {
       assertNoUnstaged(status);
       assertCodemodRan(status);
 
+      await fs.remove(path.join(tmpPath, 'package-lock.json'));
+
       // file is indeterminent between OS's, so ignore
       await fs.remove(path.join(tmpPath, 'MODULE_REPORT.md'));
+
+      // remove dist and node_modules before fixture compare
+      await run('git clean -fdX', { cwd: tmpPath });
 
       let nodeVersion = 'latest-node';
       if (process.env.NODE_LTS) {
